21f2a2ac0d42ea00c80f65e5eee30c5d498c8f9c
jonathan <jonathan.franzeli@student.umn.ac.id>
2022-01-30 14:25:17 +0700

    added mirror, enemy day sprite

--- Assets/Resources/Scripts/ChangeScene.cs
+++ Assets/Resources/Scripts/ChangeScene.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
+    void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "Startup") {
+            SceneManager.LoadScene(1);
+        }
+    }
+
     public void LoadMainMenu()
     {
         SceneManager.LoadScene(1);
